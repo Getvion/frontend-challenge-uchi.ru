f4b42fbaac329cd2026cbd4da7b40404241974c0
Oleg Somov <gog0125@mail.ru>
2023-09-21 02:54:25 +0300

    add react-router-dom and redux-toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import { App } from './App';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 import { store } from './store/store';
-import { Provider } from 'react-redux';
+import { App } from './App';
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />
-  },
-  { path: '/favorite', element: <h1> favorite</h1> }
-]);
+import './app.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
